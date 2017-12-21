ef89b402533e710ba69b4b4cd418487bcc857125
Fabio Ticconi <fabio.ticconi@gmail.com>
2017-12-21 09:17:53 +0100

    using a ranged copyright date

--- src/main/java/com/github/fabioticconi/tergen/OpenSimplexNoise.java
+++ src/main/java/com/github/fabioticconi/tergen/OpenSimplexNoise.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Fabio Ticconi
+ * Copyright (c) 2015-2017 Fabio Ticconi
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
